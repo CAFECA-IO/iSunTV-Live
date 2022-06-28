808c89951bf4d70bdfa82a8686d3aeda9fe9d0e9
Clemmy Liao <clemmyliao@host-219-71-205-104.dynamic.kbtelecom.net>
2022-06-29 00:28:47 +0800

    change the export from top line to last line

--- server/controller/chinasun.controller.ts
+++ server/controller/chinasun.controller.ts
@@ -3,23 +3,29 @@ import { ChinasunService } from '../service/chinasun.service';
 
 @Controller('chinasun')
 //export 寫下面
-export class ChinasunController {
+class ChinasunController {
   
-    chinasunService: ChinasunService;
     xlsFolder: string;
     jsonFile: string;
-
+    chinasunService: ChinasunService;
     //initialize獨立新的initial行為function
     constructor() {
-        this.chinasunService = new ChinasunService();
         this.jsonFile=process.cwd()+'/playlist.json';   
     }
- 
+
+    //initilize
+    initilize(){
+        this.chinasunService = new ChinasunService();
+    }
+
+
     @Get('updated_files')
     async getUpdated_details(){
         //show the latest data
+        this.initilize();
         const data = this.chinasunService.getUpdatedData();
         return await data;
     }
 
 }
+export {ChinasunController} ;
