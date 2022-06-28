d5632c681cf7275508b06176cf69e22198a7db5e
Clemmy Liao <clemmyliao@host-219-71-205-104.dynamic.kbtelecom.net>
2022-06-29 00:06:07 +0800

    fix the return async problem

--- server/controller/chinasun.controller.ts
+++ server/controller/chinasun.controller.ts
@@ -2,24 +2,24 @@ import { Controller, Get ,Param} from '@nestjs/common';
 import { ChinasunService } from '../service/chinasun.service'; 
 
 @Controller('chinasun')
+//export 寫下面
 export class ChinasunController {
   
-chinasunService: ChinasunService;
-xlsFolder: string;
-jsonFile: string;
+    chinasunService: ChinasunService;
+    xlsFolder: string;
+    jsonFile: string;
 
-constructor(){
-    this.chinasunService= new ChinasunService;
-    this.jsonFile=process.cwd()+'/playlist.json';
-}
-  
-@Get('updated_files')
-async getUpdated_details()
-{
-    //show the latest data
-    return await this.chinasunService.getUpdatedData()
-    
-
-}
+    //initialize獨立新的initial行為function
+    constructor() {
+        this.chinasunService = new ChinasunService();
+        this.jsonFile=process.cwd()+'/playlist.json';   
+    }
+ 
+    @Get('updated_files')
+    async getUpdated_details(){
+        //show the latest data
+        const data = this.chinasunService.getUpdatedData();
+        return await data;
+    }
 
 }
--- server/service/chinasun.service.ts
+++ server/service/chinasun.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import fs from 'fs';
 import { FormatterService } from './formatter.service';
 import xlsx from 'xlsx';
-
+// add Programlist loader 
 @Injectable()
 export class ChinasunService {
 
@@ -17,7 +17,7 @@ export class ChinasunService {
     }
     
     // loop until get the data
-    getCurrentTime():string{
+    getCurrentTime(){
 
         var currentTime = new Date();
         var month = (currentTime.getMonth() + 1);
@@ -54,85 +54,88 @@ export class ChinasunService {
     //     return this.jsonFormatter(data_json,"return default file")
     // }
 
-    getUpdatedData(){    
+    async getUpdatedData(){    
 
             // get current time 
             var now = new Date(this.getCurrentTime().slice(0, 4)+"-"+this.getCurrentTime().slice(4,6)+"-"+this.getCurrentTime().slice(6,8));
             var lastfileDate;
             var diff;
+            var files;
             var jsonFormat={
                 data: "",
                 message: ""
             }
             // get the filename
+            return new Promise(function(resolve, reject) {
+    
+                fs.readdir(process.cwd()+'/xls', function (err, filename) {
+
+                    files = filename;
             
-            fs.readdir(this.xlsFolder, function (err, files) {
                 // if the folder contains files
                 var lastfile;
-    
-                console.log(files.length);
-                if(files.length>0){
-                // get the latest file time
-                lastfile = files[files.length-1];
-                lastfile = lastfile.replace(" ","");
-                lastfile = lastfile.replace("chinasuntv.xls","");
-                lastfileDate = new Date(lastfile.slice(0, 4)+"-"+lastfile.slice(4,6)+"-"+lastfile.slice(6,8));
-    
-                diff = Math.floor(Math.abs(now.getTime()-lastfileDate.getTime())/1000/60/60/24+0.5);
-                console.log("diff");
-                console.log(diff)
-    
-                let File;
-                let Content; 
-                //check if it is the latest file(close to current time)
-                    //read file until
-                    for(let i = files.length-1 ; i >= 0;i--){
-                        console.log(i);
-                        try{
-                            File = xlsx.readFile(process.cwd()+'/xls/'+files[i],{type:'binary',cellDates:true});
-                            Content = xlsx.utils.sheet_to_json(File.Sheets[File.SheetNames[0]]);
-                            
-                            //check if it is the latest file(close to current time
-                            if((i==files.length-1)&&(diff < 7)&&(diff >= 0)){
-                                jsonFormat.data=Content;
-                                jsonFormat.message="it's the latest data";
-                                console.log(jsonFormat);
-                                return jsonFormat;
-                            }else{
-                                jsonFormat.data=Content;
-                                jsonFormat.message="it's not the latest data";
-                                console.log(jsonFormat);
-                                return jsonFormat;
-                            }
-                            
-                        }catch(e){
-                            // if return read data error => loop
-                            if(i===0){
-                                // if we can't read the last file which can be read
-                                var data = fs.readFileSync(process.cwd()+'/playlist.json',{encoding:'utf8', flag:'r'});
-                                var data_json = JSON.parse(data);
-                                jsonFormat.data=data_json;
-                                jsonFormat.message="return default file";
-                                console.log(jsonFormat);
-                                return jsonFormat;
-                            }
-                            continue;
-                        }
-                    
-                   }  
-                }else{
-                    // if the folder contains nothing
-                    var data = fs.readFileSync(process.cwd()+'/playlist.json',{encoding:'utf8', flag:'r'});
-                    var data_json = JSON.parse(data);
-                    jsonFormat.data=data_json;
-                    jsonFormat.message="return default file";
-                    console.log(jsonFormat);
-                    return jsonFormat;
-                }
-     
-            });
-
         
+                if(files.length>0){
+                    // get the latest file time
+                    lastfile = files[files.length-1];
+                    lastfile = lastfile.replace(" ","");
+                    lastfile = lastfile.replace("chinasuntv.xls","");
+                    lastfileDate = new Date(lastfile.slice(0, 4)+"-"+lastfile.slice(4,6)+"-"+lastfile.slice(6,8));
+        
+                    diff = Math.floor(Math.abs(now.getTime()-lastfileDate.getTime())/1000/60/60/24+0.5);
+        
+                    let File;
+                    let Content; 
+                    //check if it is the latest file(close to current time)
+                        //read file until
+                        for(let i = files.length-1 ; i >= 0;i--){
+            
+                            try{
+                                File = xlsx.readFile(process.cwd()+'/xls/'+files[i],{type:'binary',cellDates:true});
+                                Content = xlsx.utils.sheet_to_json(File.Sheets[File.SheetNames[0]]);
+                                //check if it is the latest file(close to current time
+                                if((i==files.length-1)&&(diff < 7)&&(diff >= 0)){
+                                    jsonFormat.data=Content;
+                                    jsonFormat.message="it's the latest data";
+                                    resolve(jsonFormat);
+                                    break;
+                                }else{
+                                    jsonFormat.data=Content;
+                                    jsonFormat.message="it's not the latest data";
+                                    console.log("it's not a latest data");
+                                    resolve(jsonFormat);
+                                    break;
+                                }
+                                
+                            }catch(e) {
+                                // if return read data error => loop
+                                if(i===0) {
+                                    // if we can't read the last file which can be read
+                                    // -> readFile (change to the async type)
+                                    var data = fs.readFileSync(process.cwd()+'/playlist.json',{encoding:'utf8', flag:'r'});
+                                    var data_json = JSON.parse(data);
+                                    jsonFormat.data = data_json;
+                                    jsonFormat.message = "return default file";
+                                    resolve(jsonFormat);
+                                    break;
+                                }
+                                continue;
+                            }
+                    }  
+
+                    }else{
+                        // if the folder contains nothing
+                        var data = fs.readFileSync(process.cwd()+'/playlist.json',{encoding:'utf8', flag:'r'});
+                        var data_json = JSON.parse(data);
+                        jsonFormat.data=data_json;
+                        jsonFormat.message="return default file";
+                        resolve(jsonFormat);
+                    }
+                });
+                // handle the error (next schedule)
+            });
+        }
+    
         // let File = xlsx.readFile(file,{type:'binary',cellDates:true});
         // let Content = xlsx.utils.sheet_to_json(File.Sheets[File.SheetNames[0]]);
         // let jsonFile=process.cwd()+'/playlist.json';
@@ -148,4 +151,4 @@ export class ChinasunService {
     }
 
 
-}
+
