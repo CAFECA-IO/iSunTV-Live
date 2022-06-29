3c1db484beae834171ac1dac28fa2eca44b3accf
Clemmy Liao <clemmyliao@host-219-71-205-104.dynamic.kbtelecom.net>
2022-06-29 10:05:35 +0800

    modify the rejeact -> reject (typo)

--- server/service/ProgramListLoader.service.ts
+++ server/service/ProgramListLoader.service.ts
@@ -12,7 +12,7 @@ class ProgramlistLoader{
         FileOperator.getFileList(path).then(
             (value)=>{
                 LastFile=value;
-                return new Promise(function(resolve, rejeact) {
+                return new Promise(function(resolve, reject) {
                     try{
                         const Sheet_json = FileOperator.readFile(LastFile[LastFile.length-1]);
                         resolve(Sheet_json);
@@ -25,7 +25,7 @@ class ProgramlistLoader{
     }
     // with specific file path?
     static getProgramList(path) {
-        return new Promise(function(resolve, rejeact) {
+        return new Promise(function(resolve, reject) {
             try{
                 const Sheet_json = FileOperator.readFile(path);
                 resolve(Sheet_json);
