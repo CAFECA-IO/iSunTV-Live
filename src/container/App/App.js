3a37366d49a8689459f893527c8f1158c97d55f4
Clemmy Liao <clemmyliao@Clemmyde-MBP.mymeshdevice.home>
2022-07-14 16:20:05 +0800

    remove video css

--- src/container/App/App.js
+++ src/container/App/App.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import './App.scss';
 import Routers from '../../components/Routers/Routers';
 
 // app create routers
 const APP = () => {
+
+  useEffect(() => {
+    document.title = '陽光衛視官方網站';
+  });
+  
   return (
     <Routers></Routers>
   );
+
 }
 
 export default APP;
