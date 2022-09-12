b9089cdc276d5cd76c1e7a5706149b1e99fff005
Clemmy Liao <clemmyliao@Clemmyde-MBP.mymeshdevice.home>
2022-09-12 16:23:02 +0800

    add email to programlist functions

--- server/utils/email_programlist.ts
+++ server/utils/email_programlist.ts
@@ -2,13 +2,15 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-var-requires */
 import Imap from 'imap';
+import fs from 'fs';
+const {Base64Decode} = require('base64-stream');
+
 const inspect = require('util').inspect;
 
 class EmailService {
   constructor() {
     // nothing to do
   }
-
   /**
    * getEmailAttachment function is used to get email attachment
    * @param user google client id
@@ -59,7 +61,7 @@ class EmailService {
 
       f.on('message', function (msg, seqno) {
         // console.log('Message #%d', seqno);
-        // const prefix = '(#' + seqno + ') ';
+        const prefix = '(#' + seqno + ') ';
 
         msg.on('body', function (stream) {
           let buffer = '';
@@ -71,6 +73,22 @@ class EmailService {
           stream.once('end', function () {
             // console.log(prefix + 'Parsed header: %s', inspect(Imap.parseHeader(buffer)));
           });
+        });
+        msg.once('attributes', function (attrs) {
+          const attachments = EmailService.getAttachmentParts(attrs.struct, []);
+          for (let i = 0, len = attachments.length; i < len; ++i) {
+            const attachment = attachments[i];
+            const f = mailServer.fetch(attrs.uid, {
+              //do not use imap.seq.fetch here
+              bodies: [attachment.partID],
+              struct: true,
+            });
+            //build function to process attachment message
+            f.on('message', EmailService.downloadAttachment(attachment));
+          }
+        });
+        msg.once('end', function () {
+          // nothing to do
         });
       });
 
@@ -86,13 +104,27 @@ class EmailService {
     return true;
   }
 
+  // test download related function
+
   /**
    * getEmailAttachment function is used to get email attachment details
    * @returns attachment list
    */
-  static getAttachmentParts(attrs: any): string[] {
-    const attr = attrs;
-    attr;
+  static getAttachmentParts(struct, attachments): any {
+    attachments = attachments || [];
+    for (let i = 0, len = struct.length, r; i < len; ++i) {
+      if (Array.isArray(struct[i])) {
+        this.getAttachmentParts(struct[i], attachments);
+      } else {
+        if (
+          struct[i].disposition &&
+          ['INLINE', 'ATTACHMENT'].indexOf(struct[i].disposition.type.toUpperCase()) > -1
+        ) {
+          attachments.push(struct[i]);
+        }
+      }
+    }
+    return attachments;
     return ['attachment_file1', 'attachment_file2'];
   }
 
@@ -101,11 +133,33 @@ class EmailService {
    * @param attachment attachment from mail
    * @returns UID
    */
-  static downloadAttachment(attachment: any): string {
-    const downloaded_attachment = attachment;
-    downloaded_attachment;
+  static downloadAttachment(attachment: any): any {
+    const filename = attachment.params.name;
+    const encoding = attachment.encoding;
 
-    return 'UID';
+    return function (msg, seqno) {
+      const prefix = '(#' + seqno + ') ';
+      msg.on('body', function (stream, info) {
+        //Create a write stream so that we can stream the attachment to file;
+        const writeStream = fs.createWriteStream(process.cwd() + '/temp/' + filename);
+        writeStream.on('finish', function () {
+          // nothing to do
+        });
+
+        //stream.pipe(writeStream); this would write base64 data to the file.
+        //so we decode during streaming using
+        if (encoding.toUpperCase() === 'BASE64') {
+          //the stream is base64 encoded, so here the stream is decode on the fly and piped to the write stream (file)
+          stream.pipe(new Base64Decode()).pipe(writeStream);
+        } else {
+          //here we have none or some other decoding streamed directly to the file which renders it useless probably
+          stream.pipe(writeStream);
+        }
+      });
+      msg.once('end', function () {
+        // nothing to do
+      });
+    };
   }
 
   /**
